b1aefa42209787e70066890a2d2ac6b24b2fdf5b
Siddami <dharmmy621@gmail.com>
2025-06-25 12:46:37 +0100

    feat: changed from fetch to fs module for data fetching

--- utils/fetchData.ts
+++ utils/fetchData.ts
@@ -1,10 +1,12 @@
+import { promises as fs } from "fs";
 import { Product } from "../types/product";
+import path from "path";
 
 export const fetchProducts = async (): Promise<Product[]> => {
   try {
-    const response = await fetch("/products.json");
-    if (!response.ok) throw new Error("Failed to fetch products");
-    return response.json();
+    const filePath = path.join(process.cwd(), "public", "products.json");
+    const data = await fs.readFile(filePath, "utf8");
+    return JSON.parse(data);
   } catch (error) {
     console.error("Error fetching products:", error);
     return [];
